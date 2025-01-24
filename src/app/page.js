62f87ec9830f8e18ebc54c1454d2dd4531a4c36b
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-24 08:44:00 +0600

    change grid layout in md devices

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ const page = async () => {
   const data = await res.json();
   // console.log(data);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
       {data.map((post) => (
         <PostCard key={post.id} post={post}></PostCard>
       ))}
