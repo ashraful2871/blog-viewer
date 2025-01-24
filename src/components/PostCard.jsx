3486fa9f411cb3ae04fb455acbcb055fa2d76b9f
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-24 10:00:09 +0600

    change home page card to title

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,17 +5,11 @@ import Link from "next/link";
 const PostCard = ({ post }) => {
   const { title, body, id } = post;
   return (
-    <Link href={`/blog/${id}`}>
-      {" "}
-      <div className="flex flex-col flex-grow h-64 border-2 border-blue-100 rounded-xl">
-        <div className="card bg-base-100 flex-grow">
-          <div className="card-body">
-            <h2 className="card-title">{title}</h2>
-            <p>{body}</p>
-          </div>
-        </div>
-      </div>
-    </Link>
+    <li className="border p-4 rounded-xl bg-gray-50 hover:scale-105 transition-all ease">
+      <Link href={`/blog/${id}`}>
+        <span className="text-blue-500 hover:underline">{title}</span>
+      </Link>
+    </li>
   );
 };
 
