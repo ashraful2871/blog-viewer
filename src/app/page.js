1139606531096afd2221e616b7ce2e61248fcfac
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-24 06:29:48 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const page = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+  console.log(data);
+  return <div>home</div>;
+};
+
+export default page;
