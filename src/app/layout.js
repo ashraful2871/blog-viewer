4334c2c74f637c0cb66227808c96a226468d65a9
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-24 09:44:41 +0600

    uncomment code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,18 @@
 "use client";
-// import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 // export const metadata = {
 //   title: "Blog-Viewer",
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <KindeProvider>
       <html lang="en">
-        <body className="max-w-[1320px] mx-auto p-3 my-2">
+        <body className="max-w-[1320px] mx-auto p-3 my-">
           <Navbar></Navbar>
           <div className="min-h-screen"> {children}</div>
         </body>
